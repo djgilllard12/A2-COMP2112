02e1c3062f2f438d25780d88e462901d921b03f5
djgilllard12 <djgillard12@live.ca>
2017-12-08 19:10:08 -0500

    adding compose

--- js/main.js
+++ js/main.js
@@ -154,4 +154,25 @@ var email1 = new Vue({
       style1.classList.add(hoverStyle);
     }
   }
-})
+});
+// adding compose functionality
+let inboxhtml = `
+<div v-on:click="clickEmail1" id="email1" class="email-item email-item-selected pure-g">
+<div class="pure-u">
+    <img width="64" height="64" alt="Tilo Mitra&#x27;s avatar" class="email-avatar" src="img/common/tilo-avatar.png">
+</div>
+
+<div class="pure-u-3-4">
+    <h5 class="email-name" id="name1">{{name1}}</h5>
+    <h4 class="email-subject" id="subject1">{{subject1}}</h4>
+    <p class="email-desc" id="message1">
+      {{ message1 }}
+    </p>
+</div>
+</div>
+`
+let click = document.getElementById('compose');
+click.addEventListener('click', function () {
+  let addEmail = document.getElementById('list');
+  addEmail.innerHTML = inboxhtml;
+});
